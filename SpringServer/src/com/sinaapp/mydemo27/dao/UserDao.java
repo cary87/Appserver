e6e8066cdba0d2ba930f52a6724047fa89eef853
Administrator <Administrator@LBDZ-20120804VS>
2014-03-22 12:08:04 +0800

    55

---
+++ SpringServer/src/com/sinaapp/mydemo27/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.sinaapp.mydemo27.dao;
+
+import java.util.List;
+
+import com.sinaapp.mydemo27.pojo.User;
+
+public interface UserDao {
+	public void deleteById(long id);
+	public void inseartUser(User user);
+	public void updateUser(User user);
+	public List<User> findByLimit(int limit);
+	public List<User> findByName(String name);
+	public User findById(long id);
+	
+}
